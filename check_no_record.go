f3ef6095f58fc17dc6a003ac76ff68ad86952719
Julia Evans <julia@jvns.ca>
2023-08-31 09:58:29 -0400

    improve check_no_record

--- check_no_record.go
+++ check_no_record.go
@@ -9,10 +9,17 @@ var CheckNoRecord = &Check{
 
 func checkNoRecord(config *Config, outputs *DigOutputs) (*CheckResult, error) {
 	last_response := outputs.trace[len(outputs.trace)-1]
-	if len(last_response.Answers) == 0 {
+	if len(outputs.trace) == 3 {
 		return &CheckResult{
 			Status:  false,
-			Message: "No record found",
+			Message: fmt.Sprintf("This domain is not registered"),
+		}, nil
+	}
+	if len(last_response.Answers) == 0 {
+		nameserver := last_response.ServerName
+		return &CheckResult{
+			Status:  false,
+			Message: fmt.Sprintf("No record found, using nameserver '%s'", nameserver),
 		}, nil
 	} else {
 		return &CheckResult{
--- check_no_record_test.go
+++ check_no_record_test.go
@@ -6,9 +6,14 @@ import (
 	"github.com/hexops/autogold/v2"
 )
 
-func TestCheckNoRecordFail(t *testing.T) {
+func TestCheckNoRecordNotRegistered(t *testing.T) {
 	got := testRunCheck(t, CheckNoRecord, "exampleffff.com", "A")
-	autogold.Expect(&CheckResult{Status: false, Message: "No record found"}).Equal(t, got)
+	autogold.Expect(&CheckResult{Message: "This domain is not registered"}).Equal(t, got)
+}
+
+func TestCheckNoRecordFail(t *testing.T) {
+	got := testRunCheck(t, CheckNoRecord, "nonexistent.example.com", "A")
+	autogold.Expect(&CheckResult{Message: "No record found, using nameserver 'a.iana-servers.net'"}).Equal(t, got)
 }
 
 func TestCheckNoRecordSucceed(t *testing.T) {
